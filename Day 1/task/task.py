ab09cdbb741a78dd7d5eeece8ed5889f8f263ddb
Sheepiii <alex.r.lopez2002@gmail.com>
2025-04-16 22:43:22 -0700

    adjusting view

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -2,4 +2,19 @@
 # then adding the ! in the end
 #adding ctrl + / will blank out the line of code
 #Doing it again will bring it back to code
-print("Hello " + input("What is your name?") + "!")
+#print("Hello " + input("What is your name?") + "!")
+
+
+name = "jack"
+print(name)
+
+name ="Angela"
+print(name)
+
+#print(len(input("What is your name?")))
+#this moves from input to print
+#first we get the input, then length, and finally print the length
+username =input("What is your name?")
+length = len(username)
+
+print(length)
