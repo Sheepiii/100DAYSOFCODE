2c7a1dd11ed28b972d5f7cb6991564c2608afa95
Sheepiii <alex.r.lopez2002@gmail.com>
2025-04-19 14:04:52 -0700

    Completed day 1

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,21 +1,4 @@
-#This is printing hello, giving the question,
-# then adding the ! in the end
-#adding ctrl + / will blank out the line of code
-#Doing it again will bring it back to code
-#print("Hello " + input("What is your name?") + "!")
-
-
-name = "jack"
-print(name)
-
-name ="Angela"
-print(name)
-
-#print(len(input("What is your name?")))
-#this moves from input to print
-#first we get the input, then length, and finally print the length
-username =input("What is your name?")
-length = len(username)
-
-print(length)
-# see how the variables flow into each other
+print("Welcome to the Band Name Generator!")
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name will be " + city +" " + pet_name + "!")
