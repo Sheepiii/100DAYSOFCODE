ba912302d522ed284f6f62c612e54fdc1da8a705
alexander <alex.r.lopez2002@gmail.com>
2025-04-26 11:49:35 -0700

    day 3

--- Day 3/If Else/task.py
+++ Day 3/If Else/task.py
@@ -1,6 +1,6 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
-if height == 120: #make sure there is a : after each if/else (also to make sure to include 120 and above user >=
+if height >= 120: #make sure there is a : after each if/else (also to make sure to include 120 and above user >=
     print("Here is your ticket god sir!")
 else:
     print("Sorry little bud")
