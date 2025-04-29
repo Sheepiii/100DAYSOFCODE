3fe4f024ba029cd108b3b83a5eb238830d131f35
alexander <alex.r.lopez2002@gmail.com>
2025-04-29 15:58:16 -0700

    day 3 part 5

--- Day 3/Nesting and Elif/task.py
+++ Day 3/Nesting and Elif/task.py
@@ -1,8 +1,16 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
-
+print("")
 if height >= 120:
     print("You can ride the rollercoaster")
+    age = int(input("What is your age? "))
+    if age <= 12:
+        print("Please pay 5.")
+    elif age <= 18:
+        print("Please pay 7.")
+    elif age <=22:
+        print("Please pay 18.")
+    else:
+        print("You cannot come into weeny hut juniors, unless you pay 100.")
 else:
     print("Sorry you have to grow taller before you can ride.")
-`
--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -1,5 +1,17 @@
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M or L: ")
+bill = 0
+size = input("What size pizza do you want? S, M or L: ").lower()
+if size == "s":
+    bill = 15
+    print(f"Your bill currently is {bill} ")
+elif size == "m":
+        bill = 20
+        print(f"Your bill currently is {bill}")
+elif size == "l":
+        bill = 25
+        print(f"Your bill currently is {bill}")
+
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
