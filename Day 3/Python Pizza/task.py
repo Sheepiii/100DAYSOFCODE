6ec1a8f10a77b47cda0ee24359ad6d5c43184b19
alexander <alex.r.lopez2002@gmail.com>
2025-04-30 15:52:13 -0700

    day 3 part 5

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -2,16 +2,30 @@ print("Welcome to Python Pizza Deliveries!")
 bill = 0
 size = input("What size pizza do you want? S, M or L: ").lower()
 if size == "s":
-    bill = 15
-    print(f"Your bill currently is {bill} ")
+   bill += 15
+   #print(f"Your bill currently is {bill} ")
 elif size == "m":
-        bill = 20
-        print(f"Your bill currently is {bill}")
+    bill += 20
+    #print(f"Your bill currently is {bill}")
 elif size == "l":
-        bill = 25
-        print(f"Your bill currently is {bill}")
+   bill += 25
+   #print(f"Your bill currently is {bill}")
+else:
+   print("You typed the wrong input")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ").lower()
+if pepperoni == "y":
+   if size == "s":
+       bill += 2
+   else:
+       bill += 3
+#print(f"Your current bill is ${bill}")
+extra_cheese = input("Do you want extra cheese? Y or N: ").lower()
+if extra_cheese == "y":
+   bill += 1
+   #print(f"Your final bill is ${bill}")
+#else:
+   #print(f"Your final bill is ${bill}!")
+print(f"Your final bill is: ${bill}.")
 
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-
-extra_cheese = input("Do you want extra cheese? Y or N: ")
-
+#All of the code greyed out will give the bill as we go through the options
+#currently the code achieves the goal of the course
