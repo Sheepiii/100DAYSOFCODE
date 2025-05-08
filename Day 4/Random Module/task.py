87daad27f1fd9feaa94bb2339825ee9ceb58dd08
alexander <alex.r.lopez2002@gmail.com>
2025-05-08 15:53:43 -0700

    Day 4 part 1

--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -1,1 +1,18 @@
-#we did this
+import random
+import my_module
+
+print(my_module.my_favorite_number)
+
+# random_number_0_to_1 = random.random() * 10
+# print(round((random_number_0_to_1),2))#this prints numbers between 0 ans 10 not including those
+#
+random_float = random.uniform(0, 10) #these will print number 1-10 including those
+print(random_float)
+
+random_integer = random.randint(0,1)
+if random_integer == 1:
+    print("heads")
+else:
+    print("Tails")
+
+print(random_integer)
